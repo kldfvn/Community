44a9a6b8eb5d5b1454da7b8a27305e1ce61d400d
ckl <1051928078@qq.com>
2022-04-13 19:21:42 +0800

    验证码功能开发

--- src/main/java/com/nowcoder/community/controller/alphacontroller.java
+++ src/main/java/com/nowcoder/community/controller/alphacontroller.java
@@ -1,14 +1,17 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.service.alphaservice;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -119,5 +122,40 @@ public class alphacontroller {
         list.add(emp1);
         list.add(emp2);
         return list;
+    }
+//    cookie
+    @RequestMapping(path = "/cookie/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response)
+    {
+        //创建
+        Cookie cookie=new Cookie("code", CommunityUtil.generateUUID());
+        //设置生效范围
+        cookie.setPath("community/alpha");
+        //设置生存实践
+        cookie.setMaxAge(60*10);
+        response.addCookie(cookie);
+        return "setCookie";
+    }
+    @RequestMapping(path = "/cookie/get",method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code)
+    {
+        return code;
+    }
+//    session
+    @RequestMapping(path = "/session/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session)
+    {
+        session.setAttribute("id",1);
+        session.setAttribute("name","test");
+        return "setSession";
+    }
+    @RequestMapping(path = "/session/get",method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session)
+    {
+        return ""+session.getAttribute("id")+"\t"+session.getAttribute("name");
     }
 }
