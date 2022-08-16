16acedfd022ee62ed1c2fd02ce29caf1a330b775
ckl <1051928078@qq.com>
2022-08-16 16:41:08 +0800

    点赞功能

--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -2,18 +2,20 @@ package com.nowcoder.community.util;
 
 public interface CommunityConstant {
     //激活成功
-    int ACTIVATION_SUCCESS=0;
+    int ACTIVATION_SUCCESS = 0;
     //重复激活
-    int ACTIVATION_REPEATE=1;
+    int ACTIVATION_REPEATE = 1;
     //激活失败
-    int ACTIVATION_FAILURE=2;
+    int ACTIVATION_FAILURE = 2;
     //默认登录凭证超时时间
-    int DEFAULT_EXPIRE_SECONDS=3600*12;
+    int DEFAULT_EXPIRE_SECONDS = 3600 * 12;
     //记住状态下的登录凭证超时时间
-    int REMEMBER_EXPIRE_SECONDS=3600*12*100;
+    int REMEMBER_EXPIRE_SECONDS = 3600 * 12 * 100;
 
-//    帖子
-    int ENTITY_TYPE_POST=1;
-//    评论
-    int ENTITY_TYPE_COMMENT=2;
+    //    帖子
+    int ENTITY_TYPE_POST = 1;
+    //    评论
+    int ENTITY_TYPE_COMMENT = 2;
+    //    用户
+    int ENTITY_TYPE_USER = 3;
 }
