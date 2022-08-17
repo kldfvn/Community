8d2d5994838df20d7f18059e1e5637324dfee57c
ckl <1051928078@qq.com>
2022-08-17 17:37:25 +0800

    Kafka实现系统通知

--- src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -29,6 +29,10 @@ public class ServiceLogAspect {
     {
 //        用户在时间访问了什么方法
         ServletRequestAttributes attributes= (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if(attributes==null)
+        {
+            return;
+        }
         HttpServletRequest request= attributes.getRequest();
         String ip=request.getRemoteHost();
         String now=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
