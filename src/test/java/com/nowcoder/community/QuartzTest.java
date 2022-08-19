19309db211eb8e6fedda2717f45491d537bb99c1
ckl <1051928078@qq.com>
2022-08-19 18:14:52 +0800

    任务执行和调度

---
+++ src/test/java/com/nowcoder/community/QuartzTest.java
@@ -0,0 +1,30 @@
+package com.nowcoder.community;
+
+import org.junit.jupiter.api.Test;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+//配置类
+@ContextConfiguration(classes = CommunityApplication.class)
+public class QuartzTest {
+
+    @Autowired
+    private Scheduler scheduler;
+
+    @Test
+    public void testDeleteJob() {
+        try {
+            boolean result = scheduler.deleteJob(new JobKey("alphaJob", "alphaJobGroup"));
+            System.out.println(result);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
