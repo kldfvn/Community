dc633ac1bd25ae15f35ce8c9b767814bd1997454
ckl <1051928078@qq.com>
2022-08-20 14:42:48 +0800

    生成长图

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -1,26 +1,89 @@
 package com.nowcoder.community.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.util.SensitiveFilter;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
+    private static Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
     @Autowired
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private SensitiveFilter sensitiveFilter;
+    @Value("${caffeine.posts.maxsize}")
+    private int maxSize;
+    @Value("${caffeine.posts.expire-seconds}")
+    private int expireSeconds;
+    //    caffeine核心接口：Cache
+//    帖子列表缓存
+    private LoadingCache<String, List<DiscussPost>> postListCache;
+    //    帖子总数缓存
+    private LoadingCache<Integer, Integer> postRowCache;
 
-    public List<DiscussPost> findDiscussPost(int userId, int offset, int limit,int orderMode) {
-        return discussPostMapper.selectDiscussPost(userId, offset, limit,orderMode);
+    @PostConstruct
+    public void init() {
+//        初始化帖子列表缓存
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Override
+                    public @Nullable List<DiscussPost> load(String key) throws Exception {
+                        if (key == null || key.length() == 0) {
+                            throw new IllegalArgumentException("参数错误");
+                        }
+                        String[] params = key.split(":");
+                        if (params == null || params.length != 2) {
+                            throw new IllegalArgumentException("参数错误");
+                        }
+                        int offset = Integer.valueOf(params[0]);
+                        int limit = Integer.valueOf(params[1]);
+//                        二级缓存：redis
+                        logger.debug("load post list from DB");
+                        return discussPostMapper.selectDiscussPost(0, offset, limit, 1);
+                    }
+                });
+//        初始化帖子总数缓存
+        postRowCache=Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Override
+                    public @Nullable Integer load(Integer integer) throws Exception {
+                        logger.debug("load post count from DB");
+                        return discussPostMapper.selectDiscussPostRows(integer);
+                    }
+                });
+    }
+
+    public List<DiscussPost> findDiscussPost(int userId, int offset, int limit, int orderMode) {
+        if (userId == 0 && orderMode == 1) {
+            return postListCache.get(offset + ":" + limit);
+        }
+        logger.debug("load post list from DB");
+        return discussPostMapper.selectDiscussPost(userId, offset, limit, orderMode);
     }
 
     public int findDiscussPostCount(int userId) {
+        if (userId == 0) {
+            return postRowCache.get(userId);
+        }
+        logger.debug("load post count from DB");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
@@ -41,21 +104,19 @@ public class DiscussPostService {
         return discussPostMapper.selectDiscussPostById(id);
     }
 
-    public int updateCommentCount(int id,int commentCount)
-    {
-        return discussPostMapper.updateCommentCount(id,commentCount);
+    public int updateCommentCount(int id, int commentCount) {
+        return discussPostMapper.updateCommentCount(id, commentCount);
     }
 
-    public int updateType(int id,int type)
-    {
-        return  discussPostMapper.updateType(id,type);
+    public int updateType(int id, int type) {
+        return discussPostMapper.updateType(id, type);
     }
-    public int updateStatus(int id,int status)
-    {
+
+    public int updateStatus(int id, int status) {
         return discussPostMapper.updateStatus(id, status);
     }
-    public int updateScore(int id,double score)
-    {
-        return discussPostMapper.updateScore(id,score);
+
+    public int updateScore(int id, double score) {
+        return discussPostMapper.updateScore(id, score);
     }
 }
